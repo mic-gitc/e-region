0b1aed6b239a99c5a5f8f01b994523dd99378df8
mic-gitc <sgayaneaslanyan@yahoo.com>
2018-01-23 16:58:56 +0400

    lesson1

---
+++ lesson1/script.js
@@ -0,0 +1,35 @@
+"use strict";
+var msg = "Hello";
+const SCREEN_COLOR = "red";
+alert(msg);
+
+for(let i = 0;  i < 10; i++){
+
+}
+document.write(i);
+
+var a;
+var b = 444;
+var c = 444.2;
+var d = +"444";
+var year = "year of";
+var arr = [4, 5, 8];
+var obj = {};
+var n = null;
+
+document.write(b +d);
+document.write("<br>");
+console.log("a", typeof a);
+console.log("b", typeof b);
+console.log("c", typeof c);
+console.log("d", typeof d);
+console.log("arr", typeof arr);
+console.log("obj", typeof obj);
+console.log("n", typeof n);
+document.write(d + " " + year);
+document.write("<br>");
+d++;
+document.write(d);
+
+var i = 5, cc = 2 * (i++) ;
+alert(i);
